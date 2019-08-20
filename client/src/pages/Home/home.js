0ae05d4f466477bfb3fcd80221bc29ba35d7757e
LeugimAtreides <m.villarreal789@hotmail.com>
2019-08-19 20:06:16 -0400

    changes to home pages

--- client/src/pages/Home/home.js
+++ client/src/pages/Home/home.js
@@ -1,5 +1,21 @@
 import React, { Component } from "react";
+
+// Components
 import Wrapper from "../../components/Wrapper";
 import Video from "../../components/Video";
 import Footer from "../../components/Footer";
+import Pill from "../../components/Pill";
+
+class Home extends Component {
+
+    render () {
+        return (
+            <Wrapper>
+                <Video />
+                    <Pill />
+                <Footer />
+            </Wrapper>
+        )
+    }
+}
 
